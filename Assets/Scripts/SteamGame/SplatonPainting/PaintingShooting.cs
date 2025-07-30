525984a0da796888d44dbc57336af7c499c5a6ce
YuyjieLiu <RCrobotcat@gmail.com>
2025-07-30 14:51:36 +0800

    Added splaton effects test

---
+++ Assets/Scripts/SteamGame/SplatonPainting/PaintingShooting.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Unity.Cinemachine;
+
+public class PaintingShooting : MonoBehaviour
+{
+    [SerializeField] ParticleSystem inkParticle;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            inkParticle.Play();
+        else if (Input.GetMouseButtonUp(0))
+            inkParticle.Stop();
+    }
+}
