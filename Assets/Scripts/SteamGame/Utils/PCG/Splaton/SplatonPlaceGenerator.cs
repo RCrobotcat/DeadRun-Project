b382c3fb7900ffb06ce9b740c3b22ea5a38b5341
YuyjieLiu <RCrobotcat@gmail.com>
2025-07-31 20:02:09 +0800

    Added splaton place marks generation logic

---
+++ Assets/Scripts/SteamGame/Utils/PCG/Splaton/SplatonPlaceGenerator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class SplatonPlaceGenerator : Singleton<SplatonPlaceGenerator>
+{
+    public int columns = 10;
+    public int rows = 10;
+
+    public float cellSize = 5f;
+
+    public SplatonCellMark cellMarkPrefab;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        GenerateGrid();
+    }
+
+    public void GenerateGrid()
+    {
+        for (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                Vector3 position = new Vector3(x * cellSize, 0, y * cellSize);
+                SplatonCellMark cellMark = Instantiate(cellMarkPrefab, position, Quaternion.identity);
+                cellMark.transform.SetParent(transform);
+            }
+        }
+    }
+}
